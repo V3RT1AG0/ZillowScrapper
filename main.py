7dd257d4d9e07e1cb885e51a0e0d68576dd40e89
V3RT1AG0 <adityarajpednekar4@gmail.com>
2019-05-19 14:33:14 -0400

    Completed For sale and rent for Houses

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ import os
 key = "X1-ZWz1h1sdyiz9jf_6v82r"
 
 
-
-
 def returnString(data):
     if data is None:
         return ""
@@ -25,6 +23,7 @@ def returnString(data):
         return data.get_text().strip()
 
 
+# Api call
 def crawl(zpid):
     print(zpid)
     returndata = dict()
@@ -41,7 +40,7 @@ def crawl(zpid):
     except requests.exceptions.TooManyRedirects as e:
         return False
     soup = BeautifulSoup(base.text, "lxml")
-    print(soup)
+    # print(soup)
     if (soup.find('address') == None):
         return None
     address = soup.find('address').contents
@@ -73,20 +72,10 @@ def crawl(zpid):
             writer.writerow(returndata)
     except IOError:
         print("I/O error")
-    # soup3 = BeautifulSoup(r.text, "html.parser")
-    # sales = soup3.find_all("tr", {"class": "zsg-table_interactive"})
-    # print(sales)
-
-    # tr in soup.find_all('tr'):
-    #     data.append([td.text for td in tr.find_all('td')])
-    #
-    # for row in data[:5]:  # Show first 5 entries
-    #     print
-    #     row
     print(returndata)
 
 
-def findLocation(location,state):
+def findLocation(location, state):
     location_url = "http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=" + key + "&address=" + location + "&citystatezip=" + state
     s = requests.Session()
     s.headers[
@@ -98,105 +87,147 @@ def findLocation(location,state):
         print(crawl(returnString(result.find('zpid'))))
 
 
-def findLocation2(zip):
-    req_headers = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'accept-encoding': 'gzip, deflate, br',
-        'accept-language': 'en-US,en;q=0.8',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
-    }
+class App:
 
-    with requests.Session() as s:
-        url = 'https://www.zillow.com/homes/for_sale/' + str(zip)
-        r = s.get(url, headers=req_headers)
-    soup = BeautifulSoup(r.content, 'lxml')
-    cards = soup.find("ul", {"class": "photo-cards"})
-    results = cards.find_all("article")
-    list = []
-    for result in results:
-        print(result)
-        returndata = dict()
-        try:
-            houseurl = "https://www.zillow.com/homes/for_sale/" + result['data-zpid'] + "_zpid"
-        except KeyError as e:
-            houseurl = "https://www.zillow.com/homes/for_sale/" + result['id'][5:] + "_zpid"
-        # h = s.get(houseurl, headers=req_headers)
-        #*****************************
+    def __init__(self):
+        self.req_headers = self.setHeaders()
+        self.driver = self.setSeleniumDriver()
+        self.findLocation(22030)
+
+    def setHeaders(self):
+        return {
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'en-US,en;q=0.8',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+        }
+
+    def setSeleniumDriver(self):
         driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
         options = webdriver.ChromeOptions()
         options.add_argument('--ignore-certificate-errors')
         options.add_argument('--incognito')
         options.add_argument('--headless')
-        # driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", chrome_options=options)
-        h = driver.get(houseurl)
-        html = driver.page_source
-        #*************************************
-        soup2 = BeautifulSoup(html, 'lxml')
-        returndata["cost"] = returnString(soup2.find("span", {"class": "ds-value"}))
-        returndata["address"]= returnString(soup2.find("h1", {"class": "ds-address-container"}))
-        returndata["summary"] = returnString(soup2.find("div", {"class": "character-count-text-fold-container"}))
-        returndata["zestimate"] = returnString(soup2.find("span", {"class": "ds-estimate-value"}))
-        facts = soup2.find("ul", {"class": "ds-home-fact-list"}).find_all("li")
-        for fact in facts:
-            label = returnString(fact.find("span", {"class": "ds-standard-label ds-home-fact-label"}))
-            value = returnString(fact.find("span", {"class": "ds-body ds-home-fact-value"}))
-            returndata[label] = value
-        print(soup2)
-        price_history = soup2.find("table", {"class":"zsg-table ds-price-and-tax-section-table"}).find_all("tr")
-        print(price_history)
-        historyList = []
-        for hs in price_history[1:]:
-            hist = dict()
-            items = hs.contents
-            hist["date"] = returnString(items[0])
-            hist["event"] = returnString(items[1])
-            hist["price"] = returnString(items[2])
-            historyList.append(hist)
-        returndata["SaleHistory"] = historyList
+        return driver
+
+    #def scrapeArticle(self):
+
+    def findLocation(self, zip):
+        # get webpage and create soup
+        with requests.Session() as s:
+            url = 'https://www.zillow.com/homes/for_sale/' + str(zip) + "_rb"
+            r = s.get(url, headers=self.req_headers)
+        soup = BeautifulSoup(r.content, 'lxml')
+        list = []
 
 
-        #WRITING TO CSV FILE
-        keys = returndata.keys()
-        try:
-            with open('zillow.csv', 'a') as csvfile:
-                writer = csv.DictWriter(csvfile, fieldnames=keys)
-                #writer.writeheader()
-                writer.writerow(returndata)
-        except IOError:
-            print("I/O error")
-        list.append(returndata)
-        driver.close()
-        print(list)
-    print(list)
+        # get number of pages
+        pages = returnString(soup.find("li", {"class", "zsg-pagination-next"}).previous_sibling)
+
+        #itereate over each page
+        for page in range(int(pages)):
+            print("PAGE:"+str(page))
+
+            #make a request for that particular page and create soup for that page
+            with requests.Session() as s:
+                url = 'https://www.zillow.com/homes/for_sale/' + str(zip) + "_rb/" + str(page) + "_p"
+                r = s.get(url, headers=self.req_headers)
+            soup = BeautifulSoup(r.content, 'lxml')
+            cards = soup.find("ul", {"class": "photo-cards"})
+
+            #find number of articles in that page and iterate over it
+            results = cards.find_all("article")
+            for result in results:
+                print(result)
+                returndata = dict()
+
+                #use selenium to load individual house article
+                try:
+                    houseurl = "https://www.zillow.com/homes/for_sale/" + result['data-zpid'] + "_zpid"
+                    returndata["zipcode"] = result['data-zpid']
+                except KeyError as e:
+                    houseurl = "https://www.zillow.com/homes/for_sale/" + result['id'][5:] + "_zpid"
+                    returndata["zipcode"] = result['id'][5:]
+                self.driver.get(houseurl)
+                html = self.driver.page_source
+
+                self.scrapeArticle()
+                soup2 = BeautifulSoup(html, 'lxml')
+                returndata["cost/rent"] = returnString(soup2.find("span", {"class": "ds-value"}))
+                returndata["status"] = returnString(soup2.find("span",{"class":"ds-status-details"}))
+                returndata["address"] = returnString(soup2.find("h1", {"class": "ds-address-container"}))
+                #finding all spans which gives bed bath and area
+                bed_bath_area = soup2.findAll("span", {"class": "ds-bed-bath-living-area"})
+                print(bed_bath_area)
+                # assigning each value in a list to a its corresponding varaible
+                returndata["bed"],returndata["bath"],returndata["area"] = [row.span.text for row in bed_bath_area][:3]
+                returndata["summary"] = returnString(soup2.find("div", {"class": "character-count-text-fold-container"}))
+                returndata["zestimate"] = returnString(soup2.find("span", {"class": "ds-estimate-value"}))
+                returndata["Principal/Interest"] = returnString(soup2.find("span",text='Principal & interest').next_sibling)
+                facts = soup2.find("ul", {"class": "ds-home-fact-list"}).find_all("li")
+                for fact in facts:
+                    label = returnString(fact.find("span", {"class": "ds-standard-label ds-home-fact-label"}))
+                    value = returnString(fact.find("span", {"class": "ds-body ds-home-fact-value"}))
+                    returndata[label] = value
 
 
+                # You might have try catch pass this whole history
+                price_history = soup2.find("table", {"class": "zsg-table ds-price-and-tax-section-table"}).find_all(
+                    "tr")
+                historyList = []
+                for hs in price_history[1:]:
+                    hist = dict()
+                    items = hs.contents
+                    hist["date"] = returnString(items[0])
+                    hist["event"] = returnString(items[1])
+                    hist["price"] = returnString(items[2])
+                    historyList.append(hist)
+                returndata["SaleHistory"] = historyList
+
+                # WRITING TO CSV FILE
+                keys = returndata.keys()
+                try:
+                    with open('zillow.csv', 'a') as csvfile:
+                        writer = csv.DictWriter(csvfile, fieldnames=keys)
+                        # writer.writeheader()
+                        writer.writerow(returndata)
+                except IOError:
+                    print("I/O error")
+                list.append(returndata)
+                # self.driver.close()
+                # print(list)
+            #print(list)
 
 
-
-#CODE TO FETCH DATA USING API INSTEAD OF SCRAPING IT FROM WEBSITE
-#findLocation("Richmond","Richmond%2C+VA")
-#findLocation("Fairfax","Fairfax%2C+VA")
+# CODE TO FETCH DATA USING API INSTEAD OF SCRAPING IT FROM WEBSITE
+# findLocation("Richmond","Richmond%2C+VA")
+# findLocation("Fairfax","Fairfax%2C+VA")
 
 
 if __name__ == "__main__":
-    window = Tk()
-    window.title("Welcome to Zillow Scraper")
-    window.geometry('350x200')
-    lbl = Label(window, text="Enter Zipcode")
-    txt = Entry(window, width=10)
-    txt.grid(column=1, row=0)
-    def clicked():
-        findLocation2(txt.get())
-        window.destroy()
-    btn = Button(window, text="Start Scraping",command=clicked)
-    btn.grid(column=1, row=1)
-    lbl.grid(column=0, row=0)
-    window.mainloop()
+    app = App()
+    # window = Tk()
+    # window.title("Welcome to Zillow Scraper")
+    # window.geometry('350x200')
+    # lbl = Label(window, text="Enter Zipcode")
+    # txt = Entry(window, width=10)
+    # txt.grid(column=1, row=0)
+    # def clicked():
+    #     findLocation2(txt.get())
+    #     window.destroy()
+    # btn = Button(window, text="Start Scraping",command=clicked)
+    # btn.grid(column=1, row=1)
+    # lbl.grid(column=0, row=0)
+    # window.mainloop()
 
-#HOW TO RUN THIS FILE:
-#1. INSTALL CHROMEDRIVER Version 74.0. AND SET EXECUTABLE PATH IN THIS CODE TO POINT THAT CHROMEDRIVER
-#2. INSTALL ALL DEPENDENCIES USING THE FOLLOWING COMMAND: pip install -r requirements.txt
-#3. SIMPLY RUN THE PYTHON PROGRAM IN THE VIRTUAL ENVIORNMENT PROVIDED i.e "venv"
-#4. ENTER THE ZIPCODE FOR WHICH YOU WANT TO FETCH THE DATA FOR IN THE PROGRAM RUNNING
-#5. THE DATA WILL BE COLLECTED IN A FILE "zillow.csv".
+# HOW TO RUN THIS FILE:
+# 1. INSTALL CHROMEDRIVER Version 74.0. AND SET EXECUTABLE PATH IN THIS CODE TO POINT THAT CHROMEDRIVER
+# 2. INSTALL ALL DEPENDENCIES USING THE FOLLOWING COMMAND: pip install -r requirements.txt
+# 3. SIMPLY RUN THE PYTHON PROGRAM IN THE VIRTUAL ENVIORNMENT PROVIDED i.e "venv"
+# 4. ENTER THE ZIPCODE FOR WHICH YOU WANT TO FETCH THE DATA FOR IN THE PROGRAM RUNNING
+# 5. THE DATA WILL BE COLLECTED IN A FILE "zillow.csv".
+
+# zillow url parameters:- /0_mmm - show only for sale items
+#https://www.zillow.com/homes/for_sale/Washington-DC-20002/house,apartment_duplex_type/66126_rid/38.953802,-76.915885,38.861765,-77.039481_rect/12_zm/
+# 1_fr,1_rs
